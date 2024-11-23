98e83616f05734c80ef4b1168f7e1f7eb3201a53
Yoann Aubry <yoann@nimbly.dev>
2024-11-23 17:36:16 +0100

    Refactor Food class: improve safety checks and update property names

--- exercise/ts/day01/src/food.ts
+++ exercise/ts/day01/src/food.ts
@@ -1,16 +1,25 @@
 type Maybe<T> = NonNullable<T> | null;
 
 export class Food {
-    constructor(
-        public readonly expirationDate: Date,
-        public readonly approvedForConsumption: boolean,
-        public readonly inspectorId: Maybe<string>
-    ) {
-    }
+  constructor(
+    public readonly expirationDate: Date,
+    public readonly isSafeForConsumption: boolean,
+    public readonly inspectorId: Maybe<string>
+  ) {}
 
-    isEdible(now: () => Date): boolean {
-        return this.expirationDate > now() &&
-            this.approvedForConsumption &&
-            this.inspectorId !== null;
-    }
+  isEdible(now: () => Date): boolean {
+    return this.isFresh(now) && this.canBeConsumed() && this.hasBeenInspected();
+  }
+
+  private canBeConsumed() {
+    return this.isSafeForConsumption;
+  }
+
+  private hasBeenInspected(): boolean {
+    return this.inspectorId !== null;
+  }
+
+  private isFresh(now: () => Date) {
+    return this.expirationDate > now();
+  }
 }
