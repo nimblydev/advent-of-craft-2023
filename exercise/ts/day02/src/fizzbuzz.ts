e613fa666e4af5f5ec933122978e8b7bf74f9087
Yoann Aubry <yoann@nimbly.dev>
2024-11-23 18:41:55 +0100

    Refactor fizzbuzz function: improve input validation and modularize logic

--- exercise/ts/day02/src/fizzbuzz.ts
+++ exercise/ts/day02/src/fizzbuzz.ts
@@ -1,20 +1,37 @@
+const MAXIMUM = 100;
+const MINIMUM = 0;
+const FIZZ = 5;
+const BUZZ = 3;
+const FIZZBUZZ = 15;
+
 export function fizzbuzz(input: number): string | number {
-    if (input > 0) {
-        if (input <= 100) {
-            if (input % 3 === 0 && input % 5 === 0) {
-                return "FizzBuzz";
-            }
-            if (input % 3 === 0) {
-                return "Fizz";
-            }
-            if (input % 5 === 0) {
-                return "Buzz";
-            }
-            return input;
-        } else {
-            throw new Error("Input is out of range");
-        }
-    } else {
-        throw new Error("Input is out of range");
-    }
+  checkInputRange(input);
+
+  if (isFizzBuzz(input)) {
+    return "FizzBuzz";
+  }
+  if (isBuzz(input)) {
+    return "Fizz";
+  }
+  if (isFizz(input)) {
+    return "Buzz";
+  }
+  return input;
 }
+
+function checkInputRange(input: number): void {
+  if (isOutOfRange(input)) {
+    throw new Error("Input is out of range");
+  }
+}
+
+const isOutOfRange = (input: number) => input <= MINIMUM || input > MAXIMUM;
+
+const isMultipleOf = (divisor: number) => (input: number) =>
+  input % divisor === 0;
+
+const isFizz = isMultipleOf(FIZZ);
+
+const isBuzz = isMultipleOf(BUZZ);
+
+const isFizzBuzz = isMultipleOf(FIZZBUZZ);
