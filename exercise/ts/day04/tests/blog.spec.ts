611e445fe7c63ad06a245ec87634ce2a17ac6f11
Yoann Aubry <yoann@nimbly.dev>
2024-11-25 23:45:49 +0100

    day04 - ts solution

--- exercise/ts/day04/tests/blog.spec.ts
+++ exercise/ts/day04/tests/blog.spec.ts
@@ -1,68 +1,43 @@
-import {Article} from '../src/blog';
+import { Article } from "../src/blog";
 
-describe('Article in a blog', () => {
-    test('should add valid comment', () => {
-        const article = new Article(
-            "Lorem Ipsum",
-            "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
+describe("Article in a blog", () => {
+  let article;
+  const author = "Pablo Escobar";
+  const commentText = "Amazing article !!!";
+  beforeEach(() => {
+    article = new Article(
+      "Lorem Ipsum",
+      "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+    );
+  });
 
-        article.addComment("Amazing article !!!", "Pablo Escobar");
-        expect(article.getComments().length).toBeGreaterThan(0);
-    });
+  test("should add comment", () => {
+    article.addComment(commentText, author);
 
-    test('should add a comment with the given text', () => {
-        const article = new Article(
-            "Lorem Ipsum",
-            "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
+    expect(article.getComments()).toHaveLength(1);
 
-        const text = "Amazing article !!!";
-        article.addComment(text, "Pablo Escobar");
+    const comment = article.getComments().at(0);
+    expect(comment.text).toBe(commentText);
+    expect(comment.author).toBe(author);
+  });
 
-        expect(article.getComments()).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({ text })
-            ])
-        );
-    });
+  test("should add a comment in an Article with an other comment", () => {
+    article.addComment(commentText, author);
+    article.addComment("Faites mieux !", "Melenchon");
 
-    test('should add a comment with the given author', () => {
-        const article = new Article(
-            "Lorem Ipsum",
-            "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
+    expect(article.getComments().length).toBeGreaterThan(1);
 
-        const author = "Pablo Escobar";
-        article.addComment("Amazing article !!!", author);
+    const newComment = article.getComments().at(1);
 
-        expect(article.getComments()).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({ author })
-            ])
-        );
-    });
+    expect(newComment.text).toEqual("Faites mieux !");
+    expect(newComment.author).toEqual("Melenchon");
+  });
 
-    test('should add a comment with the date of the day', () => {
-        const article = new Article(
-            "Lorem Ipsum",
-            "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
+  test("should throw an error when adding existing comment", () => {
+    article.addComment(commentText, author);
 
-        article.addComment("Amazing article !!!", "Pablo Escobar");
-        expect(article.getComments()[0].creationDate.toDateString()).toBe(new Date().toDateString());
-    });
-
-    test('should throw an error when adding existing comment', () => {
-        const article = new Article(
-            "Lorem Ipsum",
-            "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
-
-        article.addComment("Amazing article !!!", "Pablo Escobar");
-
-        expect(() => {
-            article.addComment("Amazing article !!!", "Pablo Escobar");
-        }).toThrow("Comment already exists");
-    });
+    expect(() => {
+      article.addComment(commentText, author);
+    }).toThrow("Comment already exists");
+  });
 });
