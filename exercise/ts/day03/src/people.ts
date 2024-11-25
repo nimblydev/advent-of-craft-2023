ce01e5900654a5d370336c6de0bb6791fd51c0b0
Yoann Aubry <yoann@nimbly.dev>
2024-11-25 23:07:55 +0100

    day03 - ts solution

--- exercise/ts/day03/src/people.ts
+++ exercise/ts/day03/src/people.ts
@@ -1,26 +1,92 @@
 export enum PetType {
-    Cat,
-    Dog,
-    Hamster,
-    Turtle,
-    Bird,
-    Snake
+  Cat,
+  Dog,
+  Hamster,
+  Turtle,
+  Bird,
+  Snake,
 }
 
 export class Pet {
-    constructor(
-        public readonly type: PetType,
-        public readonly name: string,
-        public readonly age: number
-    ) {
-    }
+  constructor(
+    public readonly type: PetType,
+    public readonly name: string,
+    public readonly age: number
+  ) {}
 }
 
 export class Person {
-    constructor(
-        public readonly firstName: string,
-        public readonly lastName: string,
-        public readonly pets: Pet[]
-    ) {
+  constructor(
+    public readonly firstName: string,
+    public readonly lastName: string,
+    private readonly _pets: Pet[]
+  ) {}
+  public get pets(): Pet[] {
+    return [...this._pets];
+  }
+}
+
+export class PetBuilder {
+  private readonly _type?: PetType;
+  private readonly _name?: string;
+  private readonly _age?: number;
+
+  private constructor(type?: PetType, name?: string, age?: number) {
+    this._type = type;
+    this._name = name;
+    this._age = age;
+  }
+
+  public static readonly aPet = (): PetBuilder => new PetBuilder();
+
+  public withType = (type: PetType): PetBuilder =>
+    new PetBuilder(type, this._name, this._age);
+
+  public withName = (name: string): PetBuilder =>
+    new PetBuilder(this._type, name, this._age);
+
+  public withAge = (age: number): PetBuilder =>
+    new PetBuilder(this._type, this._name, age);
+
+  public build(): Pet {
+    if (this._type === undefined || !this._name || this._age === undefined) {
+      console.log(this._type, this._name, this._age);
+      throw new Error("Invalid pet");
     }
+    return new Pet(this._type, this._name, this._age);
+  }
+}
+
+export class PersonBuilder {
+  private readonly _firstName?: string;
+  private readonly _lastName?: string;
+  private readonly _pets: Pet[] = [];
+
+  private constructor(firstame?: string, lastName?: string, pets?: Pet[]) {
+    this._firstName = firstame;
+    this._lastName = lastName;
+    if (pets) {
+      this._pets = [...pets];
+    }
+  }
+
+  public static readonly aPerson = (): PersonBuilder => new PersonBuilder();
+
+  public withFirstName = (firstName: string): PersonBuilder =>
+    new PersonBuilder(firstName, this._lastName, this._pets);
+
+  public withLastName = (lastName: string): PersonBuilder =>
+    new PersonBuilder(this._firstName, lastName, this._pets);
+
+  public withPet = (pet: Pet): PersonBuilder => {
+    const pets = [...this._pets, pet];
+    return new PersonBuilder(this._firstName, this._lastName, pets);
+  };
+
+  public build(): Person {
+    if (!this._firstName || !this._lastName) {
+      throw new Error("Invalid person");
+    }
+    return new Person(this._firstName, this._lastName, this._pets);
+  }
 }
--- exercise/ts/day03/tests/people.spec.ts
+++ exercise/ts/day03/tests/people.spec.ts
@@ -1,25 +1,151 @@
-import {Person, Pet, PetType} from '../src/people';
+import { Person, PersonBuilder, Pet, PetBuilder, PetType } from "../src/people";
 
-describe('population', () => {
-    let population: Person[];
+describe("population", () => {
+  let population: Person[];
 
-    beforeEach(() => {
-        population = [
-            new Person("Peter", "Griffin", [new Pet(PetType.Cat, "Tabby", 2)]),
-            new Person("Stewie", "Griffin", [new Pet(PetType.Cat, "Dolly", 3), new Pet(PetType.Dog, "Brian", 9)]),
-            new Person("Joe", "Swanson", [new Pet(PetType.Dog, "Spike", 4)]),
-            new Person("Lois", "Griffin", [new Pet(PetType.Snake, "Serpy", 1)]),
-            new Person("Meg", "Griffin", [new Pet(PetType.Bird, "Tweety", 1)]),
-            new Person("Chris", "Griffin", [new Pet(PetType.Turtle, "Speedy", 4)]),
-            new Person("Cleveland", "Brown", [new Pet(PetType.Hamster, "Fuzzy", 1), new Pet(PetType.Hamster, "Wuzzy", 2)]),
-            new Person("Glenn", "Quagmire", [])
-        ];
-    })
+  beforeEach(() => {
+    population = [
+      PersonBuilder.aPerson()
+        .withFirstName("Peter")
+        .withLastName("Griffin")
+        .withPet(
+          PetBuilder.aPet()
+            .withType(PetType.Cat)
+            .withName("Tabby")
+            .withAge(2)
+            .build()
+        )
+        .build(),
 
-    test('Lois owns the youngest pet', () => {
-        const filtered = population.sort((p1, p2) => {if ((p1.pets.sort((pet1, pet2) => pet1.age - pet2.age)[0]?.age ?? Number.MAX_VALUE) < (p2.pets.sort((pet1, pet2) => pet1.age - pet2.age)[0]?.age ?? Number.MAX_VALUE)) {return -1;}if ((p1.pets.sort((pet1, pet2) => pet1.age - pet2.age)[0]?.age ?? Number.MAX_VALUE) > (p2.pets.sort((pet1, pet2) => pet1.age - pet2.age)[0]?.age ?? Number.MAX_VALUE)) {return 1;}return 0;})[0];
+      PersonBuilder.aPerson()
+        .withFirstName("Stewie")
+        .withLastName("Griffin")
+        .withPet(
+          PetBuilder.aPet()
+            .withType(PetType.Cat)
+            .withName("Dolly")
+            .withAge(3)
+            .build()
+        )
+        .withPet(
+          PetBuilder.aPet()
+            .withType(PetType.Dog)
+            .withName("Brian")
+            .withAge(9)
+            .build()
+        )
+        .build(),
 
-        expect(filtered).not.toBeNull();
-        expect(filtered.firstName).toBe("Lois");
-    });
+      PersonBuilder.aPerson()
+        .withFirstName("Joe")
+        .withLastName("Swanson")
+        .withPet(
+          PetBuilder.aPet()
+            .withType(PetType.Dog)
+            .withName("Spike")
+            .withAge(4)
+            .build()
+        )
+        .build(),
+
+      PersonBuilder.aPerson()
+        .withFirstName("Lois")
+        .withLastName("Griffin")
+        .withPet(
+          PetBuilder.aPet()
+            .withType(PetType.Snake)
+            .withName("Serpy")
+            .withAge(1)
+            .build()
+        )
+        .build(),
+
+      PersonBuilder.aPerson()
+        .withFirstName("Meg")
+        .withLastName("Griffin")
+        .withPet(
+          PetBuilder.aPet()
+            .withType(PetType.Bird)
+            .withName("Tweety")
+            .withAge(1)
+            .build()
+        )
+
+        .build(),
+
+      PersonBuilder.aPerson()
+        .withFirstName("Chris")
+        .withLastName("Griffin")
+        .withPet(
+          PetBuilder.aPet()
+            .withType(PetType.Turtle)
+            .withName("Speedy")
+            .withAge(4)
+            .build()
+        )
+        .build(),
+
+      PersonBuilder.aPerson()
+        .withFirstName("Cleveland")
+        .withLastName("Brown")
+        .withPet(
+          PetBuilder.aPet()
+            .withType(PetType.Hamster)
+            .withName("Fuzzy")
+            .withAge(1)
+            .build()
+        )
+        .withPet(
+          PetBuilder.aPet()
+            .withType(PetType.Hamster)
+            .withName("Wuzzy")
+            .withAge(2)
+            .build()
+        )
+        .build(),
+
+      PersonBuilder.aPerson()
+        .withFirstName("Glenn")
+        .withLastName("Quagmire")
+        .build(),
+    ];
+  });
+
+  test("Lois owns the youngest pet", () => {
+    const sorted = population.toSorted(compareByYoungestPetAge);
+    const filtered = sorted.at(0);
+
+    expect(filtered).not.toBeNull();
+    expect(filtered?.firstName).toBe("Lois");
+  });
+
+  test("Lois owns the youngest pet (with stream)", () => {
+    const filteredPerson = population
+      .map((p) => ({
+        ...p,
+        youngestPetAge: Math.min(...p.pets.map((pet) => pet.age)),
+      }))
+      .toSorted((p1, p2) => p1.youngestPetAge - p2.youngestPetAge)
+      .at(0);
+
+    expect(filteredPerson).not.toBeNull();
+    expect(filteredPerson?.firstName).toBe("Lois");
+  });
 });
+
+const compareByYoungestPetAge = (p1: Person, p2: Person): number => {
+  if (getYoungestPetAge(p1) < getYoungestPetAge(p2)) {
+    return -1;
+  }
+  if (getYoungestPetAge(p1) > getYoungestPetAge(p2)) {
+    return 1;
+  }
+  return 0;
+
+  function getYoungestPetAge(p1: Person): number {
+    return p1.pets.sort(comparePetAgeAscending).at(0)?.age ?? Number.MAX_VALUE;
+  }
+};
+
+const comparePetAgeAscending = (petA: Pet, petB: Pet): number =>
+  petA.age - petB.age;
