c073da3d1387178fd35ca0b8b6f5751a47026bbc
Yoann Aubry <yoann@nimbly.dev>
2024-11-25 23:51:18 +0100

    Refactor population tests: improve readability and consistency in formatting

--- exercise/ts/day05/tests/people.spec.ts
+++ exercise/ts/day05/tests/people.spec.ts
@@ -1,71 +1,81 @@
-import {Person, Pet, PetType} from '../src/people';
+import { Person, Pet, PetType } from "../src/people";
 
-describe('population', () => {
-    let population: Person[];
+describe("population", () => {
+  let population: Person[];
 
-    beforeEach(() => {
-        population = [
-            new Person("Peter", "Griffin", [new Pet(PetType.Cat, "Tabby", 2)]),
-            new Person("Stewie", "Griffin", [new Pet(PetType.Cat, "Dolly", 3), new Pet(PetType.Dog, "Brian", 9)]),
-            new Person("Joe", "Swanson", [new Pet(PetType.Dog, "Spike", 4)]),
-            new Person("Lois", "Griffin", [new Pet(PetType.Snake, "Serpy", 1)]),
-            new Person("Meg", "Griffin", [new Pet(PetType.Bird, "Tweety", 1)]),
-            new Person("Chris", "Griffin", [new Pet(PetType.Turtle, "Speedy", 4)]),
-            new Person("Cleveland", "Brown", [new Pet(PetType.Hamster, "Fuzzy", 1), new Pet(PetType.Hamster, "Wuzzy", 2)]),
-            new Person("Glenn", "Quagmire", [])
-        ];
-    })
+  beforeEach(() => {
+    population = [
+      new Person("Peter", "Griffin", [new Pet(PetType.Cat, "Tabby", 2)]),
+      new Person("Stewie", "Griffin", [
+        new Pet(PetType.Cat, "Dolly", 3),
+        new Pet(PetType.Dog, "Brian", 9),
+      ]),
+      new Person("Joe", "Swanson", [new Pet(PetType.Dog, "Spike", 4)]),
+      new Person("Lois", "Griffin", [new Pet(PetType.Snake, "Serpy", 1)]),
+      new Person("Meg", "Griffin", [new Pet(PetType.Bird, "Tweety", 1)]),
+      new Person("Chris", "Griffin", [new Pet(PetType.Turtle, "Speedy", 4)]),
+      new Person("Cleveland", "Brown", [
+        new Pet(PetType.Hamster, "Fuzzy", 1),
+        new Pet(PetType.Hamster, "Wuzzy", 2),
+      ]),
+      new Person("Glenn", "Quagmire", []),
+    ];
+  });
 
-    test('Lois owns the youngest pet', () => {
-        const filtered = population.sort(comparePetAgeForPersons)[0];
+  test("Lois owns the youngest pet", () => {
+    const filtered = population.sort(comparePetAgeForPersons)[0];
 
-        expect(filtered).not.toBeNull();
-        expect(filtered.firstName).toBe("Lois");
-    });
+    expect(filtered).not.toBeNull();
+    expect(filtered.firstName).toBe("Lois");
+  });
 
-    test('we should be able to represent people with their pets', () => {
-        const response = formatPopulation(population);
-        expect(response).toBe("Peter Griffin who owns : Tabby " + '\n' +
-            "Stewie Griffin who owns : Dolly Brian " + '\n' +
-            "Joe Swanson who owns : Spike " + '\n' +
-            "Lois Griffin who owns : Serpy " + '\n' +
-            "Meg Griffin who owns : Tweety " + '\n' +
-            "Chris Griffin who owns : Speedy " + '\n' +
-            "Cleveland Brown who owns : Fuzzy Wuzzy " + '\n' +
-            "Glenn Quagmire")
-    });
+  test("we should be able to represent people with their pets", () => {
+    const response = formatPopulation(population);
+    expect(response).toBe(
+      "Peter Griffin who owns : Tabby" +
+        "\n" +
+        "Stewie Griffin who owns : Dolly Brian" +
+        "\n" +
+        "Joe Swanson who owns : Spike" +
+        "\n" +
+        "Lois Griffin who owns : Serpy" +
+        "\n" +
+        "Meg Griffin who owns : Tweety" +
+        "\n" +
+        "Chris Griffin who owns : Speedy" +
+        "\n" +
+        "Cleveland Brown who owns : Fuzzy Wuzzy" +
+        "\n" +
+        "Glenn Quagmire"
+    );
+  });
 });
 
 const formatPopulation = (population: Person[]): string => {
-    let response = '';
+  let response = "";
 
-    for (let i = 0; i < population.length; i++) {
-        const person = population[i];
-        response += `${person.firstName} ${person.lastName}`;
-
-        if (person.pets.length > 0) {
-            response += " who owns : ";
-            for (const pet of person.pets) {
-                response += `${pet.name} `;
-            }
-        }
-
-        if (i < population.length - 1) {
-            response += '\n';
-        }
-    }
-    return response;
+  response = population
+    .map((person) => {
+      let personInfo = `${person.firstName} ${person.lastName}`;
+      if (person.pets.length > 0) {
+        personInfo +=
+          " who owns : " + person.pets.map((pet) => pet.name).join(" ");
+      }
+      return personInfo;
+    })
+    .join("\n");
+  return response;
 };
 
 const comparePetAgeForPersons = (p1: Person, p2: Person) => {
-    if (youngestPetAgeForThePerson(p1) < youngestPetAgeForThePerson(p2)) return -1;
-    if (youngestPetAgeForThePerson(p1) <= youngestPetAgeForThePerson(p2)) return 0;
-    return 1;
-}
+  if (youngestPetAgeForThePerson(p1) < youngestPetAgeForThePerson(p2))
+    return -1;
+  if (youngestPetAgeForThePerson(p1) <= youngestPetAgeForThePerson(p2))
+    return 0;
+  return 1;
+};
 
-const youngestPetAgeForThePerson = (person: Person) => person.pets
-        .sort(ageDifference)[0]
-        ?.age
-    ?? Number.MAX_VALUE;
+const youngestPetAgeForThePerson = (person: Person) =>
+  person.pets.sort(ageDifference)[0]?.age ?? Number.MAX_VALUE;
 
 const ageDifference = (pet1: Pet, pet2: Pet) => pet1.age - pet2.age;
